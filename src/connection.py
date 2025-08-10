17cd667e20995c23a65ba173ff01e962a756542e
miku <39mkui@gmail.com>
2025-08-10 15:52:10 +0300

    -

--- src/connection.py
+++ src/connection.py
@@ -1,58 +1,32 @@
 import requests
-import urllib.parse
-import uuid
-import json
+
 from config import config
 from src.utils import HEADERS
-from src.get_subscriptions import get_subscriptions
 
-from loguru import logger
+def connect_server(server_id, sub_index=0, outbound="proxy"):
+    url = f"{config.api_url}/api/connection"
+    data = {
+        "id": server_id,
+        "_type": "subscriptionServer",
+        "sub": sub_index,
+        "outbound": outbound
+    }
+    resp = requests.post(url, headers=HEADERS, json=data)
+    return f"{'SUCCESS' if resp.status_code else {resp.text}}"
 
-def ping_server(server) -> dict:
-    whiches = [{
-        "id": server["id"],
-        "_type": server["_type"],
-        "sub": server.get("sub_index", 0)
-    }]
-    param = urllib.parse.quote(json.dumps(whiches))
-    url = f"{config.api_url}/api/httpLatency?whiches={param}"
 
-    headers = HEADERS.copy()
-    headers["X-V2raya-Request-Id"] = str(uuid.uuid4())
+def off_v2raya():
+    response = requests.delete(f"{config.api_url}/api/v2ray", headers=HEADERS)
+    data = response.json()
+    if response.status_code == 200:
+        data.get("data", {}).get("running")
+    else:
+        return False
 
-    resp = requests.get(url, headers=headers)
-    resp.raise_for_status()
-    data = resp.json()
-    logger.info(f"Пингуем {server['name']}, ответ: {json.dumps(data, indent=2, ensure_ascii=False)}")
-    return data
-
-def ping_all_servers(servers=None) -> dict:
-    results = []
-    good_servers = []
-
-    if not servers:
-        servers = get_subscriptions()
-
-    for srv in servers:
-        try:
-            data = ping_server(srv)
-
-            if data.get("code") == "SUCCESS":
-                which = data["data"]["whiches"][0]
-                latency_str = which.get("pingLatency", "").replace("ms", "").strip()
-
-                if latency_str.isdigit():
-                    latency = int(latency_str)
-                    if latency < 400:
-                        good_servers.append({
-                            "name": srv["name"],
-                            "id": srv["id"],
-                            "latency_ms": latency
-                        })
-                        logger.info(f"{srv['name']} | {srv['id']} | {latency}ms")
-
-            results.append((srv, data))
-        except Exception as e:
-            logger.error(f"Ошибка пинга сервера {srv['name']}: {e}")
-
-    return results
+def on_v2raya():
+    response = requests.post(f"{config.api_url}/api/v2ray", headers=HEADERS,json={})
+    data = response.json()
+    if response.status_code == 200:
+        data.get("data", {}).get("running")
+    else:
+        return False
