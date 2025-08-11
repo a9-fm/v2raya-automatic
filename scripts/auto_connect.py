644ed8b29819524a6f546cf141024ecf2dc555a5
A9FM [aka Artur Lamerov] <143527536+a9-fm@users.noreply.github.com>
2025-08-11 22:27:36 +0300

    Update auto_connect.py

--- scripts/auto_connect.py
+++ scripts/auto_connect.py
@@ -2,48 +2,50 @@ from src.ping import ping_all_servers
 from src.get_subscriptions import get_subscriptions
 from loguru import logger
 
-from src.connection import connect_server
+from src.connection import connect_server, on_v2raya
 
 maximal_ping = 700
 good_servers = []
 
 def get_fastest_server():
-    servers = get_subscriptions()
-    if not servers:
-        logger.warning("Нет серверов для пинга")
-        return None
+    servers = get_subscriptions()  # Получаем список серверов
 
-    data = ping_all_servers(servers)  # Пинг всех разом
-
-    good_servers = []
-    for srv, which in data:  # resp заменяем на which — это пинг по одному серверу
-        latency_str = which.get("pingLatency", "").replace("ms", "").strip()
-        if latency_str.isdigit():
-            latency = int(latency_str)
-            if latency < maximal_ping:
-                good_servers.append({
-                    "id": srv["id"],
-                    "latency_ms": latency
-                })
+    for srv in servers:
+        try:
+            data = ping_all_servers([srv])
+            if not data:
+                continue
+            _, resp = data[0]
+            if resp.get("code") == "SUCCESS":
+                which = resp["data"]["whiches"][0]
+                latency_str = which.get("pingLatency", "").replace("ms", "").strip()
+                if latency_str.isdigit():
+                    latency = int(latency_str)
+                    if latency < maximal_ping:
+                        good_servers.append({
+                            "id": srv["id"],
+                            "latency_ms": latency
+                        })
+                        print(good_servers)
+        except Exception as e:
+            logger.error(f"Ошибка при пинге сервера {srv['name']}: {e}")
 
     if not good_servers:
         return None
 
+    # Находим сервер с минимальным latency
     fastest = min(good_servers, key=lambda x: x["latency_ms"])
     return fastest
-
 
 if __name__ == "__main__":
     fastest = get_fastest_server()
     if fastest:
         logger.info(f"ID самого быстрого сервера: {fastest['id']}, задержка: {fastest['latency_ms']}ms")
         try:
-            print(fastest['id'])
-            connect_server(int(fastest['id']))
+            #on_v2raya()
+            connect_server(fastest['id'])
             logger.success("Подключили вас к серверу")
         except Exception as e:
             logger.error(f"Ошибка при подключении к серверу {fastest['id']}: {e}")
     else:
         logger.warning(f"Нет подходящих серверов с latency < {maximal_ping}ms")
-
-
